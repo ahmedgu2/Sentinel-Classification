164990ee0e749b60fefe282b31a3740156810c50
Ahmed Gueddiche <ahmed.gueddiche2@gmail.com>
2020-03-11 19:00:44 +0100

    added resnet50 model

--- model/dispatcher.py
+++ model/dispatcher.py
@@ -2,6 +2,6 @@ import model.models as models
 
 MODEL_DISPATCHER = {
     'resnet34' : models.ResNet34,
-    'inception_v3' : models.InceptionV3,
+    'resnet50' : models.ResNet50,
     
 }
--- model/models.py
+++ model/models.py
@@ -1,6 +1,6 @@
 import torch.nn as nn
 import torch
-from torchvision.models import resnet34, inception_v3, alexnet
+from torchvision.models import resnet34, inception_v3, resnet50
 
 
 class ResNet34(nn.Module):
@@ -16,14 +16,15 @@ class ResNet34(nn.Module):
         
         return self.model(x)
 
-class InceptionV3(nn.Module):
-    
-    def __init__(self, pretrained=False):
-        super(ResNet34, self).__init__()
 
-        self.model = inception_v3(pretrained=pretrained)
+class ResNet50(nn.Module):
+
+    def __init__(self, pretrained=False):
+        super(ResNet50, self).__init__()
+
+        self.model = resnet50(pretrained=pretrained)
         #changing the last layer to a fully connected one with output size 10 (number of classes)
-        self.model.fc = nn.Linear(512, 10)
+        self.model.fc = nn.Linear(2048, 10)
 
     def forward(self, x):
         
